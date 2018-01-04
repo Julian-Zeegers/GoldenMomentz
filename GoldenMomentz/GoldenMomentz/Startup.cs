dcbabf9231fd7f12d90ad38d7ce2960bd22c947d
ABJZ121 <julian.zeegers@dariel.co.za>
2018-01-04 11:15:08 +0200

    Initial Setup

---
+++ GoldenMomentz/GoldenMomentz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoldenMomentz.Startup))]
+namespace GoldenMomentz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
